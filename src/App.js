7d043f1a9eb3980658371a871290a73f3b8fa8f7
eva anna <eva.zajaczkowska@gmail.com>
2020-02-08 08:22:04 +0100

    Fixed folder structure(added files once again)

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+//Component Imports
+import Offers from './components/Offers';
+import RowOffer from './components/RowOffer';
+
+function App() {
+  return (
+    <div className="App">
+      <Offers />
+      <RowOffer />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/RowOffer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+function RowOffer(props) {
+    return(
+      <tr>
+        <th scope="row">1</th>
+            <td>01.01.2020</td>
+            <td>1</td>
+            <td>1234</td>
+      </tr>
+    );
+}
+
+export default RowOffer;
